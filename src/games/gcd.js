7c70d90441e52d975a23733e10eeb1bd28dfa8c0
Evgeny Tevelevich <eyt5297@gmail.com>
2020-09-22 17:26:10 +0700

    refactoring gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,24 +3,14 @@ import { getRandom } from '../utils.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
-const findGcd = (a, b) => {
-  if (a === b) {
-    return a;
-  }
-  const [numberBig, numberSmall] = (a > b) ? [a, b] : [b, a];
-  const remainder = numberBig % numberSmall;
-  if (remainder === 0) {
-    return numberSmall;
-  }
-  return findGcd(numberSmall, remainder);
-};
+const gcd = (x, y) => (y === 0 ? x : gcd(y, x % y));
 
 const generateRoundData = () => {
   const numberA = getRandom(0, 99);
   const numberB = getRandom(0, 99);
 
   const question = `${numberA} ${numberB}`;
-  const answer = findGcd(numberA, numberB).toString();
+  const answer = gcd(numberA, numberB).toString();
   return [question, answer];
 };
 
